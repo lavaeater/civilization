47c5d8b4bba6b50c978fdf0282410ff4ec788120
Tommie Nygren <tommie.nygren@gmail.com>
2024-08-17 11:11:36 +0200

    Added event_reader

--- src/civilization.rs
+++ src/civilization.rs
@@ -1,9 +1,9 @@
-use bevy::prelude::{Component, Event, Query, With};
+use bevy::prelude::{Component, Event, EventReader, Query, With};
 
-#[derive(Event)]
+#[derive(Event, Debug)]
 struct BeginPopulationExpansion;
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 struct Area {
     pub max_population: u8
 }
@@ -12,9 +12,16 @@ struct Area {
 Is it reasonable to have a marker component for this or will that add complexity somewhere
 else in the game? I don't knooow
  */
-#[derive(Component)]
+#[derive(Component, Debug)]
 struct AreaHasPopulation;
 
-fn expand_ppopulation(query: Query<&Area, With<AreaHasPopulation>>) {
-    
+#[derive(Component, Debug)]
+struct AreaHasSurplusPopulation;
+
+fn expand_ppopulation(
+    mut begin_event: EventReader<BeginPopulationExpansion>,
+    query: Query<&Area, With<AreaHasPopulation>>) {
+
 }
+
+fn remove_surplus_population(query: Query<&Area, With<AreaHasSurplusPopulation>>)
