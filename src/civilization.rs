6e6868f598d6256109e4c352f6c75471b2a77607
Tommie Nygren <tommie.nygren@gmail.com>
2024-08-18 11:39:39 +0200

    Added event handler to move tokens from area to area.

--- src/civilization.rs
+++ src/civilization.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::{Component, Entity, Event, EventReader, Query, With};
+use bevy::prelude::{BuildChildren, Children, Commands, Component, Entity, Event, EventReader, Query, With};
+use bevy_console::Command;
 use crate::player::Player;
 
 #[derive(Event, Debug)]
@@ -8,12 +9,24 @@ struct BeginPopulationExpansion;
 struct MoveTokensFromStockToArea {
     pub area_entity: Entity,
     pub player_entity: Entity,
-    pub number_of_tokens: u8
+    pub number_of_tokens: u8,
+}
+
+#[derive(Event, Debug)]
+struct MoveTokenFromAreaToArea {
+    pub from_area: Entity,
+    pub to_area: Entity,
+    pub tokens: Vec<Entity>,
+}
+
+#[derive(Event, Debug)]
+struct CheckAreaSurplus {
+    pub area: Entity,
 }
 
 #[derive(Component, Debug)]
 struct Area {
-    pub max_population: u8
+    pub max_population: u8,
 }
 
 /***
@@ -26,20 +39,38 @@ struct AreaHasPopulation;
 #[derive(Component, Debug)]
 struct AreaHasSurplusPopulation;
 
-fn expand_ppopulation(
+fn move_token_from_area_to_area(
+    mut move_events: EventReader<MoveTokenFromAreaToArea>,
+    mut commands: Commands,
+) {
+    for ev in move_events.read() {
+        commands.entity(ev.from_area).remove_children(&ev.tokens);
+        commands.entity(ev.to_area).push_children(&ev.tokens);
+    }
+}
+
+fn expand_population(
     mut begin_event: EventReader<BeginPopulationExpansion>,
     area_query: Query<&Area, With<AreaHasPopulation>>,
-    player_query: Query<&Player>
+    player_query: Query<&Player>,
 ) {
+    /*
+    q_parent: Query<(&MySquadDamage, &Children)>,
+        q_child: Query<&MyUnitHealth>,
+    ) {
+        // get the properties of each squad
+        for (squad_dmg, children) in q_parent.iter() {
+            // `children` is a collection of Entity IDs
+            for &child in children.iter() {
+                // get the health of each child unit
+                let health = q_child.get(child);
 
+                // do something
+     */
 }
 
 fn move_tokens_from_stock_to_area(
     mut move_event: EventReader<MoveTokensFromStockToArea>,
-) {
-    
-}
+) {}
 
-fn remove_surplus_population(query: Query<&Area, With<AreaHasSurplusPopulation>>) {
-
-}
+fn remove_surplus_population(query: Query<&Area, With<AreaHasSurplusPopulation>>) {}
