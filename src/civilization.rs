d1b4cb710cf5457292187720097ae99a671be175
Tommie Nygren <tommie.nygren@gmail.com>
2024-08-18 13:43:46 +0200

    Potentially I have indeed managed to create a method that checks population for an area and sends appropriate events if that area has a population and needs to be expanded.
    Should work with tokens from multiple players in the area.

--- src/civilization.rs
+++ src/civilization.rs
@@ -1,6 +1,6 @@
-use bevy::prelude::{BuildChildren, Children, Commands, Component, Entity, Event, EventReader, Query, With};
-use bevy_console::Command;
 use crate::player::Player;
+use bevy::prelude::{BuildChildren, Children, Commands, Component, Entity, Event, EventReader, EventWriter, Query, With};
+use itertools::Itertools;
 
 #[derive(Event, Debug)]
 struct BeginPopulationExpansion;
@@ -29,6 +29,17 @@ struct Area {
     pub max_population: u8,
 }
 
+#[derive(Component, Debug)]
+struct Stock;
+
+#[derive(Component, Debug)]
+struct Population;
+
+#[derive(Component, Debug)]
+struct Token {
+    pub player: Entity,
+}
+
 /***
 Is it reasonable to have a marker component for this or will that add complexity somewhere
 else in the game? I don't knooow
@@ -49,28 +60,46 @@ fn move_token_from_area_to_area(
     }
 }
 
+/***
+A system that checks if an area has children... I mean, this is completely unnecessary really
+ */
+fn check_population() {}
+
 fn expand_population(
     mut begin_event: EventReader<BeginPopulationExpansion>,
-    area_query: Query<&Area, With<AreaHasPopulation>>,
-    player_query: Query<&Player>,
+    area_query: Query<(Entity, &Children), With<Population>>,
+    token_query: Query<&Token>,
+    mut event_writer: EventWriter<MoveTokensFromStockToArea>,
 ) {
-    /*
-    q_parent: Query<(&MySquadDamage, &Children)>,
-        q_child: Query<&MyUnitHealth>,
-    ) {
-        // get the properties of each squad
-        for (squad_dmg, children) in q_parent.iter() {
-            // `children` is a collection of Entity IDs
-            for &child in children.iter() {
-                // get the health of each child unit
-                let health = q_child.get(child);
-
-                // do something
-     */
+    for _event in begin_event.read() {
+        for (population_entity, population_tokens) in area_query.iter() {
+            if population_tokens.iter().count() > 0 {
+                for (player, tokens) in population_tokens
+                    .iter()
+                    .chunk_by(|pop_ent| {
+                        token_query.get(**pop_ent).unwrap().player
+                    }).into_iter() {
+                    match tokens.count() {
+                        1 => {
+                            event_writer.send(MoveTokensFromStockToArea {
+                                area_entity: population_entity,
+                                player_entity: player,
+                                number_of_tokens: 1,
+                            });
+                        }
+                        2 => {
+                            event_writer.send(MoveTokensFromStockToArea {
+                                area_entity: population_entity,
+                                player_entity: player,
+                                number_of_tokens: 2,
+                            });
+                        },
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
 }
-
-fn move_tokens_from_stock_to_area(
-    mut move_event: EventReader<MoveTokensFromStockToArea>,
-) {}
 
 fn remove_surplus_population(query: Query<&Area, With<AreaHasSurplusPopulation>>) {}
