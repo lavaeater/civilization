2bd0413bcd361a2440f25a577a5ed5ffcd9b0bad
Tommie Nygren <tommie.nygren@gmail.com>
2024-08-25 10:21:08 +0200

    WIP movement.

--- src/civilization/movement.rs
+++ src/civilization/movement.rs
@@ -1,26 +1,28 @@
 use bevy::app::App;
 use crate::civilization::civ::{GameActivity, GameActivityStarted};
-use bevy::prelude::{Event, EventReader, Plugin, Reflect};
+use bevy::prelude::{Event, EventReader, EventWriter, Plugin, Reflect};
 
 pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_event()
+            .add_event::<PrepareNextMoverCommand>()
         ;
     }
 }
 
 #[derive(Event, Debug, Reflect)]
-pub struct DetermineNextMoverCommand {}
+pub struct PrepareNextMoverCommand {}
 
 pub fn start_movement_activity(
     mut start_activity: EventReader<GameActivityStarted>,
+    mut next_mover_command: EventWriter<PrepareNextMoverCommand>
 ) {
     for activity in start_activity.read() {
         if activity.0 == GameActivity::Movement {
-
+            next_mover_command.send(PrepareNextMoverCommand {});
         }
     }
 }
+
