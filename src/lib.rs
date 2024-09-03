8c5f7717546d944e5c3d18a398a4b864dca2096b
Tommie Nygren <tommie.nygren@gmail.com>
2024-09-03 16:00:31 +0200

    Created config.toml for parallell compilation

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,6 @@ use bevy::app::App;
 // use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 
-// This example game uses States to separate logic
-// See https://bevy-cheatbook.github.io/programming/states.html
-// Or https://github.com/bevyengine/bevy/blob/main/examples/ecs/state.rs
 #[derive(States, Clone, PartialEq, Eq, Hash, Debug, Default)]
 pub enum GameState {
     // During the loading State the LoadingPlugin will load our assets
