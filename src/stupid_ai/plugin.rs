fa9e5d249b9f07dc7872d697a20c66d3e9a7ea62
Tommie Nygren <tommie.nygren@gmail.com>
2024-09-07 09:00:01 +0200

    Adds boilerplate for stupid ai systems.

--- src/stupid_ai/plugin.rs
+++ src/stupid_ai/plugin.rs
@@ -1,5 +1,6 @@
-use bevy::app::Plugin;
-use bevy::prelude::{App, Component, OnEnter, Reflect};
+use bevy::app::{Plugin, Update};
+use bevy::prelude::{in_state, App, Commands, Component, IntoSystemConfigs, OnEnter, Reflect};
+use crate::civilization::game_phases::game_activity::GameActivity;
 use crate::GameState;
 
 pub struct StupidAiPlugin;
@@ -7,7 +8,13 @@ pub struct StupidAiPlugin;
 impl Plugin for StupidAiPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(OnEnter(GameState::Playing), ())
+            .add_systems(OnEnter(GameState::Playing), setup_stupid_ai)
+            .add_systems(Update, (
+                move_tokens.run_if(in_state(GameActivity::Movement)),
+                build_cities.run_if(in_state(GameActivity::CityConstruction)),
+                remove_surplus.run_if(in_state(GameActivity::RemoveSurplusPopulation)),
+                reduce_cities.run_if(in_state(GameActivity::CheckCitySupport)),
+            ))
         ;
     }
 
@@ -16,3 +23,26 @@ impl Plugin for StupidAiPlugin {
 #[derive(Component, Debug, Reflect)]
 pub struct StupidAi;
 
+
+// Run different systems depending on what game state we are in...
+fn setup_stupid_ai(
+    mut commands: Commands,
+) {
+    
+}
+
+fn move_tokens() {
+    
+}
+
+fn build_cities() {
+    
+}
+
+fn remove_surplus() {
+    
+}
+
+fn reduce_cities() {
+    
+}
