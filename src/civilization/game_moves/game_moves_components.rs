f9a3c545a40003526569589be5838c13b53c99b2
Tommie Nygren <tommie.nygren@gmail.com>
2024-09-17 22:16:26 +0200

    to make the enum thingie for moves more readable, I decided to make the variants use a struct.

--- src/civilization/game_moves/game_moves_components.rs
+++ src/civilization/game_moves/game_moves_components.rs
@@ -16,5 +16,29 @@ impl AvailableMoves {
 
 #[derive(Debug, Clone, Copy)]
 pub enum Move {
-    PopulationExpansion(Entity, usize),
+    PopulationExpansion(PopExpMove),
+    Movement(MovementMove),
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct MovementMove {
+    pub source: Entity,
+    pub target: Entity,
+    pub player: Entity,
+    pub max_tokens: usize
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct PopExpMove {
+    pub area: Entity,
+    pub max_tokens: usize,
+}
+
+impl PopExpMove {
+    pub fn new(area: Entity, max_tokens: usize) -> Self {
+        PopExpMove {
+            area,
+            max_tokens
+        }
+    }
 }
