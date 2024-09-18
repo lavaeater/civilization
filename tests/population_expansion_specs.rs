42a7ef551841989afa02ecb83813f0cf0815e3d5
Tommie Nygren <tommie.nygren@gmail.com>
2024-09-18 15:17:26 +0200

    Removed rspec because it bring enough to the table

--- tests/population_expansion_specs.rs
+++ tests/population_expansion_specs.rs
@@ -1,5 +1,3 @@
-extern crate rspec;
-
 mod common;
 
 use bevy::app::Update;
@@ -13,70 +11,60 @@ use crate::common::{create_area, setup_bevy_app, setup_player};
 
 #[test]
 fn calculate_game_moves_in_population_expansion() {
-    #[derive(Clone, Default, Debug)]
-    struct Environment;
-    rspec::run(&rspec::given("game state PopulationExpansion", Environment::default(), |ctx| {
-        ctx.when("a player with token in three areas", |ctx| {
-            ctx.when("he does not have enought tokens for expansion", |ctx| {
-                ctx.then("the correct moves are created", |_ctx| {
-                    let mut app = setup_bevy_app(|mut app| {
-                        app
-                            .add_event::<RecalculatePlayerMoves>()
-                            .add_systems(Update, recalculate_pop_exp_moves_for_player)
-                        ;
-                        app
-                    });
+    let mut app = setup_bevy_app(|mut app| {
+        app
+            .add_event::<RecalculatePlayerMoves>()
+            .add_systems(Update, recalculate_pop_exp_moves_for_player)
+        ;
+        app
+    });
 
-                    let (player, mut tokens, _city_tokens) = setup_player(&mut app, "Player 1", GameFaction::Egypt);
+    let (player, mut tokens, _city_tokens) = setup_player(&mut app, "Player 1", GameFaction::Egypt);
 
-                    let mut player_areas = PlayerAreas::default();
-                    let mut stock = PlayerStock::new(47, tokens.drain(0..4).collect());
+    let mut player_areas = PlayerAreas::default();
+    let mut stock = PlayerStock::new(47, tokens.drain(0..4).collect());
 
-                    let area_one = create_area(&mut app, "Egypt");
-                    let area_two = create_area(&mut app, "Thrace");
-                    let area_three = create_area(&mut app, "Crete");
-                    let areas = vec![area_one, area_two, area_three];
-                    for area in areas.iter() {
-                        let mut population = Population::new(4);
-                        let token = stock.remove_token_from_stock().unwrap();
-                        player_areas.add_token_to_area(*area, token);
-                        population.add_token_to_area(player, token);
-                        app
-                            .world_mut()
-                            .entity_mut(*area)
-                            .insert(population);
-                    }
+    let area_one = create_area(&mut app, "Egypt");
+    let area_two = create_area(&mut app, "Thrace");
+    let area_three = create_area(&mut app, "Crete");
+    let areas = vec![area_one, area_two, area_three];
+    for area in areas.iter() {
+        let mut population = Population::new(4);
+        let token = stock.remove_token_from_stock().unwrap();
+        player_areas.add_token_to_area(*area, token);
+        population.add_token_to_area(player, token);
+        app
+            .world_mut()
+            .entity_mut(*area)
+            .insert(population);
+    }
 
-                    app.world_mut()
-                        .entity_mut(player)
-                        .insert((player_areas, stock));
+    app.world_mut()
+        .entity_mut(player)
+        .insert((player_areas, stock));
 
 
-                    let mut events = app.world_mut()
-                        .resource_mut::<Events<RecalculatePlayerMoves>>();
+    let mut events = app.world_mut()
+        .resource_mut::<Events<RecalculatePlayerMoves>>();
 
-                    events.send(RecalculatePlayerMoves::new(player));
+    events.send(RecalculatePlayerMoves::new(player));
 
-                    // Act
-                    app.update();
-                    // Assert
-                    let player_moves = app
-                        .world()
-                        .entity(player)
-                        .get::<AvailableMoves>();
-                    assert!(player_moves.is_some());
-                    let player_moves = player_moves.unwrap();
-                    assert_eq!(player_moves.moves.len(), 3);
-                    for (_move_index, p_move) in player_moves.moves.iter() {
-                        assert!(matches!(p_move, Move::PopulationExpansion(..)));
-                        if let Move::PopulationExpansion(pop_exp) = p_move {
-                            assert_eq!(pop_exp.max_tokens, 1);
-                        };
-                    }
-                });
-            });
-        });
-    }));
+    // Act
+    app.update();
+    // Assert
+    let player_moves = app
+        .world()
+        .entity(player)
+        .get::<AvailableMoves>();
+    assert!(player_moves.is_some());
+    let player_moves = player_moves.unwrap();
+    assert_eq!(player_moves.moves.len(), 3);
+    for (_move_index, p_move) in player_moves.moves.iter() {
+        assert!(matches!(p_move, Move::PopulationExpansion(..)));
+        if let Move::PopulationExpansion(pop_exp) = p_move {
+            assert_eq!(pop_exp.max_tokens, 1);
+        };
+    }
 }
 
 #[test]
