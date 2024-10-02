950e6bc92034b319c9365d30e6f2aabb95d6cc63
Tommie Nygren <tommie.nygren@gmail.com>
2024-10-02 16:01:24 +0200

    Progress being made on how to do all this...

--- src/civilization/conflict/conflict_systems.rs
+++ src/civilization/conflict/conflict_systems.rs
@@ -1,26 +1,22 @@
-use bevy::prelude::{debug, Commands, Entity, EventWriter, Has, NextState, Query, ResMut, With};
-use bevy::core::Name;
-use bevy_console::PrintConsoleLine;
 use crate::civilization::conflict::conflict_components::UnresolvedConflict;
 use crate::civilization::general::general_components::{BuiltCity, Population};
 use crate::civilization::general::general_events::ReturnTokenToStock;
 use crate::GameActivity;
+use bevy::core::Name;
+use bevy::prelude::{debug, Commands, Entity, EventWriter, Has, NextState, Query, ResMut, With};
+use bevy_console::PrintConsoleLine;
 
 pub fn resolve_conflicts(
     mut conflict_zones: Query<(Entity, &Name, &mut Population, Has<BuiltCity>), With<UnresolvedConflict>>,
     mut return_token: EventWriter<ReturnTokenToStock>,
     mut commands: Commands) {
-    for (area_entity, _name, mut population, has_city) in conflict_zones.iter_mut() {
+    for (area_entity, _name, mut population, _has_city) in conflict_zones.iter_mut() {
         let temp_map = population.player_tokens.clone();
         let mut players = temp_map.keys().copied().collect::<Vec<Entity>>();
         players.sort_by(|a, b| temp_map[b].len().cmp(&temp_map[a].len()));
 
         if population.max_population == 1 {
-            if population.number_of_players() == 2 {
-                handle_city_conflict(&mut players, &mut population, &mut return_token);
-            } else {
-                debug!("WEIRD CORNER CASE AAAH!");
-            }
+            handle_max_pop_is_one_conflicts(&mut players, &mut population, &mut return_token);
         } else if population.all_lengths_equal() {
             handle_all_lengths_equal(&players, &mut population, &mut return_token);
         } else {
@@ -34,7 +30,7 @@ pub fn resolve_conflicts(
 fn handle_all_lengths_equal(
     players: &Vec<Entity>,
     population: &mut Population,
-    return_token: &mut EventWriter<ReturnTokenToStock>
+    return_token: &mut EventWriter<ReturnTokenToStock>,
 ) {
     let mut token_rounds = 1;
     let must_remove = population.total_population() - population.max_population;
@@ -52,51 +48,65 @@ fn handle_all_lengths_equal(
 fn handle_unequal_lengths(
     players: &mut Vec<Entity>,
     population: &mut Population,
-    return_token: &mut EventWriter<ReturnTokenToStock>
+    return_token: &mut EventWriter<ReturnTokenToStock>,
 ) {
-    while population.total_population() > population.max_population {
+    // Sort players by their token count (from most to least)
+    players.sort_by_key(|b| std::cmp::Reverse(population.population_for_player(*b)));
+
+    // We will continue to remove tokens until only the player with the most tokens has non-zero tokens
+    while players.len() > 1 {
         let current_player = players.pop().unwrap();
+
+        // Remove 1 token from the current player
         for token in population.remove_tokens_from_area(current_player, 1).unwrap_or_default() {
             return_token.send(ReturnTokenToStock::new(token));
         }
-        if population.has_player(current_player) {
-            players.insert(0, current_player); // Put the player back in the queue if they still have tokens
+
+        // Check if the current player still has tokens
+        if population.population_for_player(current_player) > 0 {
+            players.insert(0, current_player); // Put back in the queue if they still have tokens
+        }
+
+        // If only one player remains in the list, stop the process
+        if players.len() == 1 && population.population_for_player(players[0]) > 0 {
+            break;
         }
     }
 }
 
-fn handle_city_conflict(
+fn handle_max_pop_is_one_conflicts(
     players: &mut Vec<Entity>,
     population: &mut Population,
-    return_token: &mut EventWriter<ReturnTokenToStock>
+    return_token: &mut EventWriter<ReturnTokenToStock>,
 ) {
-    let player_one = players.pop().unwrap();
-    let player_two = players.pop().unwrap();
+    // Sort players by their population size (from highest to lowest)
+    players.sort_by(|a, b| population.population_for_player(*b).cmp(&population.population_for_player(*a)));
 
+    // If all players have the same number of tokens
     if population.all_lengths_equal() {
-        for token in population.remove_all_but_n_tokens(player_one, 0).unwrap_or_default() {
-            return_token.send(ReturnTokenToStock::new(token));
-        }
-        for token in population.remove_all_but_n_tokens(player_two, 0).unwrap_or_default() {
-            return_token.send(ReturnTokenToStock::new(token));
-        }
-    } else if population.population_for_player(player_one) > population.population_for_player(player_two) {
-        for token in population.remove_all_but_n_tokens(player_one, 2).unwrap_or_default() {
-            return_token.send(ReturnTokenToStock::new(token));
-        }
-        for token in population.remove_all_but_n_tokens(player_two, 0).unwrap_or_default() {
-            return_token.send(ReturnTokenToStock::new(token));
+        // Remove all tokens from every player
+        for player in players.iter() {
+            for token in population.remove_all_but_n_tokens(*player, 0).unwrap_or_default() {
+                return_token.send(ReturnTokenToStock::new(token));
+            }
         }
     } else {
-        for token in population.remove_all_but_n_tokens(player_one, 0).unwrap_or_default() {
+        // Find the player with the highest population
+        let largest_player = players[0];
+
+        // Remove all but 2 tokens from the player with the largest population
+        for token in population.remove_all_but_n_tokens(largest_player, 2).unwrap_or_default() {
             return_token.send(ReturnTokenToStock::new(token));
         }
-        for token in population.remove_all_but_n_tokens(player_two, 2).unwrap_or_default() {
-            return_token.send(ReturnTokenToStock::new(token));
+
+        // Remove all tokens from all other players
+        for player in players.iter().skip(1) { // Skip the largest player
+            for token in population.remove_all_but_n_tokens(*player, 0).unwrap_or_default() {
+                return_token.send(ReturnTokenToStock::new(token));
+            }
         }
     }
 }
-
 
 pub fn find_conflict_zones(
     pop_query: Query<(Entity, &Name, &Population, Has<BuiltCity>)>,
--- tests/conflict_tests.rs
+++ tests/conflict_tests.rs
@@ -65,7 +65,7 @@ fn given_two_players_in_an_area_with_too_much_population_area_is_marked_as_confl
     mut write_line: EventWriter<PrintConsoleLine>,
  */
 
-pub struct ConflictTestStruct {
+pub struct TwoPlayerTestStruct {
     pub player_one_tokens: usize,
     pub player_two_tokens: usize,
     pub area_max_population: usize,
@@ -73,18 +73,56 @@ pub struct ConflictTestStruct {
     pub player_two_expected: usize,
 }
 
-impl ConflictTestStruct {
+impl TwoPlayerTestStruct {
+    fn expected(&self) -> usize {
+        self.player_one_expected + self.player_two_expected
+    }
+    
     fn new(player_one_tokens: usize,
            player_two_tokens: usize,
            area_max_population: usize,
            player_one_expected: usize,
            player_two_expected: usize) -> Self {
-        ConflictTestStruct {
+        TwoPlayerTestStruct {
             player_one_tokens,
             player_two_tokens,
             area_max_population,
             player_one_expected,
             player_two_expected,
+        }
+    }
+}
+
+pub struct ThreePlayerTestStruct {
+    pub player_one_tokens: usize,
+    pub player_two_tokens: usize,
+    pub player_three_tokens: usize,
+    pub area_max_population: usize,
+    pub player_one_expected: usize,
+    pub player_two_expected: usize,
+    pub player_three_expected: usize,
+}
+
+impl ThreePlayerTestStruct {
+    fn expected(&self) -> usize {
+        self.player_one_expected + self.player_two_expected + self.player_three_expected
+    }
+    
+    fn new(player_one_tokens: usize,
+           player_two_tokens: usize,
+           player_three_tokens: usize,
+           area_max_population: usize,
+           player_one_expected: usize,
+           player_two_expected: usize,
+           player_three_expected: usize) -> Self {
+        ThreePlayerTestStruct {
+            player_one_tokens,
+            player_two_tokens,
+            player_three_tokens,
+            area_max_population,
+            player_one_expected,
+            player_two_expected,
+            player_three_expected,
         }
     }
 }
@@ -104,19 +142,19 @@ fn when_resolving_conflicts_the_correct_result_is_obtained() {
         .add_systems(Update, resolve_conflicts);
 
     let test_cases = vec![
-        ConflictTestStruct::new(1, 1, 1, 0, 0),
-        ConflictTestStruct::new(2, 1, 1, 2, 0),
-        ConflictTestStruct::new(2, 1, 2, 2, 0),
-        ConflictTestStruct::new(2, 2, 2, 1, 1),
-        ConflictTestStruct::new(3, 2, 2, 2, 0),
-        ConflictTestStruct::new(3, 2, 3, 2, 1),
-        ConflictTestStruct::new(3, 3, 3, 1, 1),
-        ConflictTestStruct::new(4, 2, 3, 3, 0),
-        ConflictTestStruct::new(3, 3, 4, 2, 2),
-        ConflictTestStruct::new(4, 1, 4, 4, 0),
-        ConflictTestStruct::new(4, 2, 4, 3, 1),
-        ConflictTestStruct::new(4, 3, 4, 3, 1),
-        ConflictTestStruct::new(5, 3, 4, 3, 1),
+        TwoPlayerTestStruct::new(1, 1, 1, 0, 0),
+        TwoPlayerTestStruct::new(2, 1, 1, 2, 0),
+        TwoPlayerTestStruct::new(2, 1, 2, 2, 0),
+        TwoPlayerTestStruct::new(2, 2, 2, 1, 1),
+        TwoPlayerTestStruct::new(3, 2, 2, 2, 0),
+        TwoPlayerTestStruct::new(3, 2, 3, 2, 1),
+        TwoPlayerTestStruct::new(3, 3, 3, 1, 1),
+        TwoPlayerTestStruct::new(4, 2, 3, 3, 0),
+        TwoPlayerTestStruct::new(3, 3, 4, 2, 2),
+        TwoPlayerTestStruct::new(4, 1, 4, 4, 0),
+        TwoPlayerTestStruct::new(4, 2, 4, 3, 1),
+        TwoPlayerTestStruct::new(4, 3, 4, 3, 1),
+        TwoPlayerTestStruct::new(5, 3, 4, 3, 1),
     ];
 
     let (player_one, mut player_one_tokens, _) = setup_player(&mut app, "player one", GameFaction::Egypt);
@@ -146,12 +184,73 @@ fn when_resolving_conflicts_the_correct_result_is_obtained() {
         let population = app.world().get::<Population>(area).unwrap();
         assert_eq!(
             population.total_population(),
-            test_case.player_two_expected + test_case.player_one_expected,
+            test_case.expected(),
             "Conflict resolution with p1: {}, p2: {}, max_pop: {}, exp: {}",
             test_case.player_one_tokens,
             test_case.player_two_tokens,
             test_case.area_max_population,
-            test_case.player_one_expected + test_case.player_two_expected
+            test_case.expected()
+        );
+    }
+}
+
+#[test]
+fn given_three_conflicteers_the_correct_result_is_obtained() {
+    // Arrange
+    let mut app = App::new();
+    app
+        .add_plugins(
+            StatesPlugin,
+        )
+        .add_event::<ReturnTokenToStock>()
+        .add_event::<PrintConsoleLine>()
+        .insert_state(GameState::Playing)
+        .add_sub_state::<GameActivity>()
+        .add_systems(Update, resolve_conflicts);
+
+    let test_cases = vec![
+        ThreePlayerTestStruct::new(1, 1, 1, 1, 0, 0, 0),
+        ThreePlayerTestStruct::new(4, 2, 1, 3, 3, 0, 0),
+        ThreePlayerTestStruct::new(4, 4, 4, 3, 1, 1, 1),
+        ThreePlayerTestStruct::new(2, 5, 1, 2, 1, 1, 1),
+    ];
+
+    let (player_one, mut player_one_tokens, _) = setup_player(&mut app, "player one", GameFaction::Egypt);
+
+    let (player_two, mut player_two_tokens, _) = setup_player(&mut app, "player two", GameFaction::Crete);
+    let (player_three, mut player_three_tokens, _) = setup_player(&mut app, "player three", GameFaction::Babylon);
+
+    for test_case in test_cases {
+        let mut population = Population::new(test_case.area_max_population);
+
+        population.player_tokens.insert(player_one, player_one_tokens.drain(0..test_case.player_one_tokens).collect());
+        population.player_tokens.insert(player_two, player_two_tokens.drain(0..test_case.player_two_tokens).collect());
+        population.player_tokens.insert(player_three, player_three_tokens.drain(0..test_case.player_three_tokens).collect());
+
+        let area = app.world_mut().spawn(
+            (
+                Name::new("egypt"),
+                GameArea::new(1),
+                LandPassage::default(),
+                UnresolvedConflict,
+                population
+            )
+        ).id();
+
+
+        // Act
+        app.update();
+        // Assert
+        let population = app.world().get::<Population>(area).unwrap();
+        assert_eq!(
+            population.total_population(),
+            test_case.expected(),
+            "Conflict resolution with p1: {}, p2: {}, p3: {}, max_pop: {}, exp: {}",
+            test_case.player_one_tokens,
+            test_case.player_two_tokens,
+            test_case.player_three_tokens,
+            test_case.area_max_population,
+            test_case.expected()
         );
     }
 }
