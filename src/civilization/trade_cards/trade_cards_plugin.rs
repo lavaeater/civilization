75d4c19f2cebb31a6374626382916686e06fb0fe
Tommie Nygren <tommie.nygren@gmail.com>
2024-10-15 22:24:39 +0200

    Add more tradecard stuff.

--- src/civilization/trade_cards/trade_cards_plugin.rs
+++ src/civilization/trade_cards/trade_cards_plugin.rs
@@ -1,15 +1,33 @@
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::{App, Commands, Component, OnEnter, Plugin, Query, Resource};
+use bevy::utils::{HashMap, HashSet};
+use crate::civilization::census::prelude::GameInfoAndStuff;
+use crate::civilization::general::prelude::*;
+use crate::GameActivity;
 
 pub struct TradeCardsPlugin;
 
 impl Plugin for TradeCardsPlugin {
     fn build(&self, app: &mut App) {
         app
-            // .add_systems(
-            //     // OnEnter(GameActivity::AcquireTradeCards), find_trade_card_zones)
-            //     // .add_systems(Update, (trade_card_gate).run_if(in_state(GameActivity::AcquireTradeCards)))
-            //     // .observe(on_add_unresolved_trade_card)
-            // )
+            .add_systems(
+                 OnEnter(GameActivity::AcquireTradeCards), acquire_trade_cards)
         ;
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct TradeCard;
+
+#[derive(Resource, Debug)]
+pub struct TradeCards {
+    pub card_piles: HashMap<usize, HashSet<TradeCard>>
+}
+
+fn acquire_trade_cards(
+    player_query: Query<&PlayerCities>,
+    mut commands: Commands,
+) {
+    for player_cities in player_query.iter().sort_by::<&PlayerCities>(|v1, v2| v1.number_of_cities().cmp(&v2.number_of_cities())) {
+        
     }
 }
