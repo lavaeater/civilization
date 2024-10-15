3fd24186f3a00f8127f792879ed3f1beed42b043
Tommie Nygren <tommie.nygren@gmail.com>
2024-10-15 15:04:48 +0200

    Removes the spritebundle.

--- src/civilization/general/general_triggers.rs
+++ src/civilization/general/general_triggers.rs
@@ -1,5 +1,6 @@
 use crate::civilization::general::prelude::*;
 use bevy::prelude::{Commands, Component, OnAdd, Query, Trigger};
+use bevy::sprite::SpriteBundle;
 
 #[derive(Debug, Component)]
 pub struct ReturnTokenToStock;
@@ -12,7 +13,9 @@ pub fn on_add_return_token_to_stock(
 ) {
     commands
         .entity(trigger.entity())
-        .remove::<ReturnTokenToStock>();
+        .remove::<ReturnTokenToStock>()
+        .remove::<SpriteBundle>()
+    ;
     if let Ok(token) = token_query.get(trigger.entity()) {
         if let Ok ((mut token_stock, mut player_areas)) = player_query.get_mut(token.player()) {
             return_token_to_stock(trigger.entity(), &mut token_stock, &mut player_areas);
