db916147007ca03dfd3e7360c2f52498da8e7b0e
Tommie Nygren <tommie.nygren@gmail.com>
2024-11-06 20:37:57 +0100

    Gloooorp

--- src/civilization/game_moves/plugins.rs
+++ src/civilization/game_moves/plugins.rs
@@ -1,5 +1,5 @@
 use crate::civilization::game_moves::events::RecalculatePlayerMoves;
-use crate::civilization::game_moves::systems::{recalculate_city_construction_moves_for_player, recalculate_city_support_moves_for_player, recalculate_movement_moves_for_player, recalculate_pop_exp_moves_for_player};
+use crate::civilization::game_moves::systems::{recalculate_city_construction_moves_for_player, recalculate_city_support_moves_for_player, recalculate_movement_moves_for_player, recalculate_pop_exp_moves_for_player, recalculate_trade_moves_for_player};
 use crate::civilization::game_moves::triggers::{on_add_has_just_moved, on_add_has_too_many_cities, on_add_is_building, on_add_manual_expansion, on_add_perform_movement};
 use crate::GameActivity;
 use bevy::prelude::{in_state, App, IntoSystemConfigs, Plugin, Update};
