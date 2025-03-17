2d5e7fe4995a7c3fa4a17fc3747dc3c0f755196b
Tommie Nygren <tommie.nygren@gmail.com>
2025-03-17 22:41:26 +0100

    Reverintg Bevy 0.15 change that screwed up my life!

--- build.rs
+++ build.rs
@@ -5,6 +5,6 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("windows") {
         // on windows we will set our game icon as icon for the executable
-        let _ = embed_resource::compile("build/windows/icon.rc", None::<&str>);
+        let _ = embed_resource::compile("build/windows/icon.rc");
     }
 }
