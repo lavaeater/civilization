a87f77f995dd61317417967ba6c486bc1b413537
Tommie Nygren <tommie.nygren@gmail.com>
2025-03-17 22:00:00 +0100

    Added bevy_hui to crates

--- tests/trade_tests.rs
+++ tests/trade_tests.rs
@@ -3,3 +3,21 @@ mod common;
 /**
 Identify some nifty test cases for us here. 
  */
+#[test]
+fn start_game() {
+    let mut app = setup_bevy_app(|mut app| {
+        app
+            .add_event::<CheckPlayerCitySupport>()
+            .add_systems(Update, start_check_city_support)
+        ;
+        app
+    });
+
+    setup_player(&mut app, "Player 1", GameFaction::Egypt);
+    create_area(&mut app, "Egypt", 1);
+
+    app.update();
+
+    let state = app.world().get_resource::<NextState<GameActivity>>().unwrap();
+    assert!(matches!(state, Pending(GameActivity::AcquireTradeCards)));
+}
