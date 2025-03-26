4575a31c8f03a37d202d4cfbcd63ee1c7c1b44c3
Tommie Nygren <tommie.nygren@gmail.com>
2025-03-26 09:01:41 +0100

    Fixed the only place I use remove::<Sprite>() to also remove required components.

--- src/civilization/triggers/general_triggers.rs
+++ src/civilization/triggers/general_triggers.rs
@@ -1,7 +1,6 @@
-use bevy::prelude::{Commands, Component, OnAdd, Query, Trigger};
-use bevy::sprite::SpriteBundle;
 use crate::civilization::components::prelude::*;
 use crate::civilization::functions::prelude::return_token_to_stock;
+use bevy::prelude::{Commands, Component, OnAdd, Query, Sprite, Trigger};
 
 #[derive(Debug, Component)]
 pub struct ReturnTokenToStock;
@@ -15,7 +14,7 @@ pub fn on_add_return_token_to_stock(
     commands
         .entity(trigger.entity())
         .remove::<ReturnTokenToStock>()
-        .remove::<SpriteBundle>()
+        .remove_with_requires::<Sprite>()
     ;
     if let Ok(token) = token_query.get(trigger.entity()) {
         if let Ok ((mut token_stock, mut player_areas)) = player_query.get_mut(token.player()) {
