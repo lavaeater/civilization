5f7326d11b500da0ca09b8e8b5f8ebec56bdc498
Tommie Nygren <tommie.nygren@gmail.com>
2025-03-27 23:01:54 +0100

    Added the Belly UI plugin and started working on the UI.

--- src/civilization/plugins/mod.rs
+++ src/civilization/plugins/mod.rs
@@ -4,13 +4,14 @@ pub mod conflict_plugin;
 pub mod general_plugin;
 pub mod movement_plugin;
 pub mod remove_surplus_plugin;
+mod belly_ui_plugin;
 
 pub mod prelude {
     pub use crate::civilization::concepts::trade::plugins::*;
+    pub use crate::civilization::game_moves::plugins::*;
     pub use crate::civilization::plugins::city_construction_plugin::*;
     pub use crate::civilization::plugins::city_support_plugin::*;
     pub use crate::civilization::plugins::conflict_plugin::*;
-    pub use crate::civilization::game_moves::plugins::*;
     pub use crate::civilization::plugins::movement_plugin::*;
     pub use crate::civilization::plugins::remove_surplus_plugin::*;
 }
